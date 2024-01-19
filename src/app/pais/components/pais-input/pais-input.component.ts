499df6e1368d898e8a85b3488c1cf965704e9594
Gerson Santos <41873993+jherx@users.noreply.github.com>
2024-01-19 06:31:41 -0600

    fix: component input

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 @Component({
@@ -10,27 +10,35 @@ import { debounceTime } from 'rxjs/operators';
 export class PaisInputComponent implements OnInit  {
 
 
-
-  @Output() onEnter  : EventEmitter<string> = new  EventEmitter();
-  @Output() onDebounce:EventEmitter<string> = new  EventEmitter();
-
+  @Input()  public placeholder:string = ''
+  @Output() public onEnter  : EventEmitter<string> = new  EventEmitter();
+  @Output() public onDebounce:EventEmitter<string> = new  EventEmitter();
+  
+  deboncerSuscription?:Subscription
   debouncer: Subject<string> = new  Subject();
   termino:string = ''
 
   constructor() { }
   
   ngOnInit() {
-   this.debouncer.pipe(debounceTime(300)).subscribe(valor =>{
+   this.deboncerSuscription = this.debouncer.pipe(debounceTime(3000)).subscribe(valor =>{
+    // console.log('valor', valor)
    this.onDebounce.emit(valor)
-    
    })
   }
 
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.deboncerSuscription?.unsubscribe( )
+  }
    buscar(){
     this.onEnter.emit(this.termino)
    }
  
-   keypress(){
-   this.debouncer.next(this.termino)
+   keypress(searchTerm:string){
+    console.log(searchTerm)
+   this.debouncer.next(searchTerm)
    }
 }
